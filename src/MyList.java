a6448e0641e1396872874915d793c0846bd894d3
LorcanBrophy <20112489@mail.wit.ie>
2026-01-07 06:36:45 +0000

    Initial commit (made MyList interface)

---
+++ src/MyList.java
@@ -0,0 +1,14 @@
+public interface MyList<T> {
+
+    void add(T element);
+
+    T get(int index);
+
+    T remove(int index);
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+}
---
+++ src/MySinglyLinkedList.java
@@ -0,0 +1,48 @@
+public class MySinglyLinkedList<T> implements MyList<T> {
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> Tail;
+    private int size;
+
+
+
+    @Override
+    public void add(T element) {
+
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
